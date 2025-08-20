70f1b6c1fb34ee6b19dc4b6be0d5ff816cc7d476
Alberto Marques <bento.dev@hotmail.com>
2025-08-20 14:01:36 -0300

    Add files via upload

---
+++ area_retangulo.py
@@ -0,0 +1,7 @@
+print ("Olá, vamos calcular a área do retângulo!")
+
+base_retangulo = float(input ("Digite a base do retângulo: "))
+altura_retangulo = float(input ("Digite a altura do retângulo: "))
+
+area_retangulo = (base_retangulo * altura_retangulo)
+print ("A área do retângulo é: ", area_retangulo)
